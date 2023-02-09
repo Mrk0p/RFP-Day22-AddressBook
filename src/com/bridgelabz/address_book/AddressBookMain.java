76b1199e57a501aa0487633bdc6b26b8c7e281b2
Mrk0p <sachinwele23@gmail.com>
2023-02-09 22:25:43 +0530

    Added:Add multiple person in Address book system

--- src/com/bridgelabz/address_book/AddressBookMain.java
+++ src/com/bridgelabz/address_book/AddressBookMain.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.address_book;
 
 import java.util.ArrayList;
-import java.util.ListIterator;
 import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        ArrayList<AddressBook> book = new ArrayList<>();
+        ArrayList<AddressBook> addressBook = new ArrayList<>();
+        AddressBook book = new AddressBook();
         Scanner input = new Scanner(System.in);
         int option;
         do{
@@ -14,50 +14,39 @@ public class AddressBookMain {
             System.out.print("1.Add\t");
             System.out.print("2.Edit\t");
             System.out.print("3.Display\t");
-            System.out.print("4.Exit\n");
+            System.out.print("4.Delete\t");
+            System.out.print("5.Exit\n");
             System.out.print("Enter an option from above :- ");
             option = input.nextInt();
 
             switch (option){
+
                 case 1 :
-                    AddressBook addressBook = new AddressBook();
-                    addressBook.getContact();
-                    book.add(addressBook);
-                    System.out.println(book);
+                    System.out.println("Enter the number of contact you want ot add : ");
+                    int numberOfContacts = input.nextInt();
+                    for (int i =0; i < numberOfContacts ; i++){
+                        book.getContactDetails();
+                    }
+                    addressBook.add(book);
+                    System.out.println(addressBook);
                     break;
                 case 2 :
-                    System.out.println("Enter a name");
-                    String name = input.next();
-                    ListIterator<AddressBook> iterator = book.listIterator();
-                    while (iterator.hasNext()){
-                        addressBook = iterator.next();
-                        if(name.equals(addressBook.person.getFirstName())){
-                            addressBook.editPersonDetails();
-                            iterator.set(addressBook);
-                        }
-                    }
-                    System.out.println(book);
+                    book.editPersonDetails();
+
                     break;
                 case 3 :
                     System.out.println("Displaying Array : ");
-                    System.out.println(book);
+                    System.out.println(addressBook);
                     break;
                 case 4 :
-                    System.out.println("Enter a name");
-                    String name1 = input.next();
-                    ListIterator<AddressBook> iterator1 = book.listIterator();
-                    while (iterator1.hasNext()){
-                        addressBook = iterator1.next();
-                        if(name1.equals(addressBook.person.getFirstName())){
-                            iterator1.remove();
-                        }
-                    }
+                    book.deleteContact();
                     break;
                 case 5 :
                     System.out.println("Exiting from book");
                     break;
+
             }
-        }while (option != 4);
+        }while (option != 5);
     }
 }
 
--- src/com/bridgelabz/address_book/Contact.java
+++ src/com/bridgelabz/address_book/Contact.java
@@ -108,6 +108,7 @@ public class Contact {
         System.out.print("Enter Email ID : ");
         email = input.next();
     }
+
     @Override
     public String toString() {
         return "Contact{" +
